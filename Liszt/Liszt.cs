e14270fdbdd02b26aacf6deec375399751b77d6d
Laust <laust.bonnesen@mail.com>
2022-04-19 23:15:28 +0200

    Always adds on the same added index.

--- Liszt/Liszt.cs
+++ Liszt/Liszt.cs
@@ -11,81 +11,73 @@
 
 public class Liszt<T>
 {
+    // Attributes
     public static long Version {get{return 101;}}
 
     private Dictionary<string,T> _dictionary { get; set; } 
     private T[] _inventory { get; set; }
     
     public int Size {get{return _inventory.Length-1;}}
-    private int _identicalKeys { get; set; } public int IdenticalKeys { get{return _identicalKeys;} }
 
-    public Liszt() { InitiateFields(); }
-    public Liszt(T[] initialElements)
-    {
-        InitiateFields();
-        Add(initialElements);
-    }
-    private void InitiateFields()
+    private void InitiateAttributes()
     {
         _inventory = new T[1];
         _dictionary = new Dictionary<string,T>();
-        _identicalKeys = 0;
+    }
+    
+    // Constructors
+    public Liszt() { InitiateAttributes(); }
+    public Liszt(T[] initialElements)
+    {
+        InitiateAttributes();
+        Add(initialElements);
     }
 
     // Adds an amount of elements in front of the Liszt
     public T[] Add(T[] elements)
     {
-        T[] newInventory = _inventory;
-        Array.Resize(ref newInventory,_inventory.Length+elements.Length);
-        
-        for (int i = 0; i < elements.Length; i++)
-        {
-            newInventory[_inventory.Length + i] = elements[i];
-            _dictionary.Add(CreateDictionaryKey(elements[i]),elements[i-Size]);
-        }
-        _inventory = newInventory;
-
+        for (int i = 0; i < elements.Length; i++) { Add(elements[i]); }
         return _inventory;
     }
     // Adds an amount of elements in front of the Liszt
     public T[] Add(T element)
     {
-        T[] newInventory = _inventory;
-        Array.Resize(ref newInventory,_inventory.Length+1);
-        
-        newInventory[_inventory.Length] = element;
-        _dictionary.Add(CreateDictionaryKey(element),element);
-
-        _inventory = newInventory;
-        
+        _inventory = CreateNewInventory(element);
         return _inventory;
+    }
+    private T[] CreateNewInventory(T element)
+    {
+        // Temporally variable 
+        T[] newInventory = _inventory;
+        Array.Resize(ref newInventory,newInventory.Length+1);
+        
+        // Adding
+        newInventory[_inventory.Length] = element;
+        _dictionary.Add(DictionaryKey(element),element);
+
+        return newInventory;
+    }
+    private string DictionaryKey(T element)
+    {
+        if (element == null) {return "NULL";}
+        else {return element.ToString();}
     }
 
     public T[] AddSpareData(T element)
     {
         _inventory[0] = element;
-        _dictionary.Add("SpareData",element);
+        _dictionary.Add("SPAREDATA",element);
         return _inventory;
     }
 
-    private string CreateDictionaryKey(T element)
-    {
-        if (Contains(element.ToString()))
-        {
-            _identicalKeys++;
-            return "This is identical key number " + _identicalKeys;
-        }
-        return element.ToString();
-    }
-    
     public T Get(T element) { return _dictionary[element.ToString()]; }
     public T Get(string key) { return _dictionary[key]; }
     public T Get(int index) { return _inventory[index]; }
     public T GetDecremented(int index) { return _inventory[index+1];}
-    public T GetSpareData() {return _dictionary["SpareData"];}
+    public T GetSpareData() {return _dictionary["SPAREDATA"];}
     public T[] GetInventory() {return _inventory;}
 
-    public void ClearAll() { InitiateFields(); }
+    public void ClearAll() { InitiateAttributes(); }
 
     public bool Contains(T element)
     {
